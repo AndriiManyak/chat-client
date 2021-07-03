5d270124950623b9064a92524297686f6db25609
VoduDay <andriy.maniak@gmail.com>
2021-07-03 15:12:48 +0300

    add ChatBody and ChatSidebar components

---
+++ src/components/ChatBody/ChatBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatBody = () => {
+    return (
+        <section className="body">
+
+        </section>
+    );
+};
+
+export default ChatBody;
---
+++ src/components/ChatSidebar/ChatSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatSidebar = () => {
+    return (
+        <aside className="sidebar">
+
+        </aside>
+    );
+};
+
+export default ChatSidebar;
